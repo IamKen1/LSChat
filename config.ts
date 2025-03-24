cb88dd5d429c1ed56f418eddd67f566e85013932
kbgutierrez <mr.gutierrezkenneth@gmail.com>
2025-03-25 07:59:55 +0800

    go live of master

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.0.157:5000';
-// export const API_BASE_URL = 'https://msg.lemonsquare.com.ph';
+// export const API_BASE_URL = 'http://192.168.0.157:5000';
+export const API_BASE_URL = 'https://msg.lemonsquare.com.ph';
